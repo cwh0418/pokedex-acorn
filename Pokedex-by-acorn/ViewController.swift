8e7d4e1320d9b12a43a301fdb07fb594ede36d06
cwh0418 <Woonghee@jounghuiui-macbook-pro.local>
2016-02-10 15:36:20 +0900

    Initial Commit

---
+++ Pokedex-by-acorn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-by-acorn
+//
+//  Created by 조웅희 on 2016. 2. 10..
+//  Copyright © 2016년 Sweet Acorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
