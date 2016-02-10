202926d9b96871201c28c5e6d221d8a98f419564
cwh0418 <Woonghee@jounghuiui-MacBook-Pro.local>
2016-02-11 01:38:08 +0900

    Searbar implementation

---
+++ Pokedex-by-acorn/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex-by-acorn
+//
+//  Created by 조웅희 on 2016. 2. 10..
+//  Copyright © 2016년 Sweet Acorn. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}
---
+++ Pokedex-by-acorn/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex-by-acorn
+//
+//  Created by 조웅희 on 2016. 2. 10..
+//  Copyright © 2016년 Sweet Acorn. All rights reserved.
+//
+
+import Foundation
